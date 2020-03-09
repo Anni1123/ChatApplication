d3363b5ed336bd4007cd019c943a0aff25fe6c38
Anni1123 <kumarianni1213@gmail.com>
2020-03-09 13:29:48 +0530

    Fourth commit

---
+++ app/src/main/java/com/example/chatapplication/Register.java
@@ -0,0 +1,65 @@
+package com.example.chatapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Register extends AppCompatActivity {
+
+    private EditText regnamee;
+    private EditText regemaill;
+    private EditText regpasss;
+    private Button regbutt;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+        regnamee=(EditText)findViewById(R.id.regname);
+        regemaill=(EditText)findViewById(R.id.regemail);
+        regpasss=(EditText)findViewById(R.id.regpass);
+        regbutt =(Button) findViewById(R.id.regbutton);
+
+        regbutt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name=regnamee.getText().toString();
+                String email=regemaill.getText().toString();
+                String pass=regpasss.getText().toString();
+                reg_user(name,email,pass);
+            }
+        });
+    }
+    private void reg_user(String name,String email,String password){
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+
+                    Intent mainIntent=new Intent(Register.this,MainActivity.class);
+                    startActivity(mainIntent);
+                    finish();
+                }
+                else{
+                    Toast.makeText(Register.this,"You got some error",Toast.LENGTH_LONG);
+
+                }
+            }
+        });
+    }
+
+}
--- app/src/main/java/com/example/chatapplication/StartActivity.java
+++ app/src/main/java/com/example/chatapplication/StartActivity.java
@@ -2,13 +2,25 @@ package com.example.chatapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
 
+    private Button regbut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        regbut=(Button)findViewById(R.id.reg);
+        regbut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent regIntent=new Intent(StartActivity.this,Register.class);
+                startActivity(regIntent);
+            }
+        });
     }
 }
