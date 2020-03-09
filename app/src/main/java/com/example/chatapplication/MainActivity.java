942c91eafcdf8653bfe9476efd818a570d2411d9
Anni1123 <kumarianni1213@gmail.com>
2020-03-09 14:37:32 +0530

    Fifth commit

--- app/src/main/java/com/example/chatapplication/MainActivity.java
+++ app/src/main/java/com/example/chatapplication/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.chatapplication;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
 
+    private FirebaseAuth firebaseAuth;
+    private Toolbar mtoolbar;
 
 
     @Override
@@ -17,15 +24,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
+      mtoolbar=(Toolbar)findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mtoolbar);
+        getSupportActionBar().setTitle("APP");
     }
     @Override
     public void onStart() {
         super.onStart();
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if(currentUser==null){
-            Intent startIntent=new Intent(MainActivity.this,StartActivity.class);
-            startActivity(startIntent);
-            finish();
+           senttostart();
         }
+    }
+    public void senttostart(){
+        Intent startIntent=new Intent(MainActivity.this,StartActivity.class);
+        startActivity(startIntent);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.mainmenu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if(item.getItemId()==R.id.menulogout){
+
+            FirebaseAuth.getInstance().signOut();
+            senttostart();
+        }
+        return true;
     }
 }
